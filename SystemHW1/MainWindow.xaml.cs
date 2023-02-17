af6fadc7a84f2232fc1fe82bc322773f9abc120d
Rəsul <71267982+ResuIl@users.noreply.github.com>
2023-02-17 18:41:49 +0400

    Add project files.

---
+++ SystemHW1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace SystemHW1;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Process> Processes { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+        Processes = new(Process.GetProcesses());
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        if (lv_Processes.SelectedItem is null)
+            return;
+
+        if (lv_Processes.SelectedItem is Process process)
+        {
+            try
+            {
+                process.Kill();
+                Processes.Remove(process);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        
+        if (!string.IsNullOrWhiteSpace(tBox_ProcessName.Text))
+            Process.Start(tBox_ProcessName.Text);
+    }
+}
